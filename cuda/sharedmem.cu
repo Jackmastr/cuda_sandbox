55482df8d3a7b6e4ff6bda0047b54e35a44d9301
Jackson <jsipple@berkeley.edu>
2018-05-29 19:52:01 -0700

    starting to work with shared memory

---
+++ cuda/sharedmem.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+__global__ void staticReverse(int *d, int n)
+{
+	__shared__ int s[64];
+	int t = threadIdx.x;
+	int tr = n - t - 1;
+	s[t] = d[t];
+	__syncthreads();
+	d[t] = s[tr];
+}
