941d94783ceb5bf3291a0314b7289769fa271db0
Jackson <jsipple@berkeley.edu>
2018-06-04 12:48:14 -0700

    I thought I got streams to work with cublas, yet there is no real speedup. I must be doing something wrong but I cant figure out what

--- skcuda/sksaxpy.py
+++ skcuda/sksaxpy.py
@@ -8,25 +8,41 @@ s = cuda.Event()
 e = cuda.Event()
 s.record()
 
-n = np.int32(1e8)
+N = np.int32(1e8)
 a = np.float32(2)
 
-x = np.ones(n, dtype=np.float32)
-y = 2.*np.ones(n, dtype=np.float32)
+x = np.ones(N, dtype=np.float32)
+y = 2.*np.ones(N, dtype=np.float32)
 
-x_pin = cuda.register_host_memory(x)
-y_pin = cuda.register_host_memory(y)
+nStreams = 1
+streams = [cuda.Stream() for i in range(nStreams)]
 
-x_gpu = gpuarray.to_gpu(x_pin)
-y_gpu = gpuarray.to_gpu(y_pin)
+x_pin = [cuda.register_host_memory(x[i*N/nStreams:(i+1)*N/nStreams]) for i in range(nStreams)]
+y_pin = [cuda.register_host_memory(y[i*N/nStreams:(i+1)*N/nStreams]) for i in range(nStreams)]
 
 h = cublas.cublasCreate()
-cublas.cublasSaxpy(h, x_gpu.size, a, x_gpu.gpudata, 1, y_gpu.gpudata, 1)
+
+x_gpu = np.empty(nStreams, dtype=object)
+y_gpu = np.empty(nStreams, dtype=object)
+ans = np.empty(nStreams, dtype=object)
+
+for i in range(nStreams):
+	cublas.cublasSetStream(h, streams[i].handle)
+	
+	x_gpu[i] = gpuarray.to_gpu_async(x_pin[i], stream=streams[i])
+	y_gpu[i] = gpuarray.to_gpu_async(y_pin[i], stream=streams[i]) 
+
+	cublas.cublasSaxpy(h, x_gpu[i].size, a, x_gpu[i].gpudata, 1, y_gpu[i].gpudata, 1)
+	ans[i] = y_gpu[i].get_async(stream=streams[i])
+
+
 cublas.cublasDestroy(h)
 
 
 # Uncomment to check for errors in the calculation
-# np.allclose(y_gpu.get(), a*x + y)
+#y_gpu = np.array([yg.get() for yg in y_gpu])
+#y_gpu = np.array(y_gpu).reshape(y.shape)
+#print np.allclose(y_gpu, a*x + y)
 
 e.record()
 e.synchronize()
