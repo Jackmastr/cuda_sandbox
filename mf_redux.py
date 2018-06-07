5c08a48ce2d16f39ed6acca2521e15c37c93a864
Jackson <jsipple@berkeley.edu>
2018-06-07 15:04:17 -0700

    Began writing test cases when I ran into a problem I never had seen before. Somehow, using less than the full 1024 blocksize of the GPU I run out of resources (meaning registers???) when I use a 9x9 median window, but not, say 11x11 or even 19x19. This may be a problem I cannot fix, so I think I will have to just continue forward with a generic 16x16=256 blocksize?

--- mf_redux.py
+++ mf_redux.py
@@ -5,131 +5,140 @@ from pycuda.compiler import SourceModule
 import numpy as np 
 import scipy.signal as sps
 
-BLOCK_WIDTH = 32
-BLOCK_HEIGHT = 32
 
-WINDOW_SIZE = 5
+def MedianFilter(ws=3, bw=32, bh=32, n=256, indata=None):
 
-padding = WINDOW_SIZE/2
+	BLOCK_WIDTH = bw
+	BLOCK_HEIGHT = bh
 
-N = np.int32(256)
+	WINDOW_SIZE = ws
 
-#indata = np.array([[2, 80, 6, 3], [2, 80, 6, 3], [2, 80, 6, 3], [2, 80, 6, 3]], dtype=np.float32)
-indata_orig = np.array(np.random.rand(N, N), dtype=np.float32)
+	padding = WINDOW_SIZE/2
 
-s = cuda.Event()
-e = cuda.Event()
-s.record()
+	N = np.int32(n)
 
+	#indata = np.array([[2, 80, 6, 3], [2, 80, 6, 3], [2, 80, 6, 3], [2, 80, 6, 3]], dtype=np.float32)
+	if indata is None:
+		indata = np.array(np.random.rand(N, N), dtype=np.float32)
+	else:
+		indata = np.array(indata, dtype=np.float32)
 
-code = """
-
-	// X is the 2D image
-	const int BW = %(BW)s;
-	const int BH = %(BH)s;
-	const int WS = %(WS)s;
-
-	__global__ void mf(float* in, float* out, int imgWidth, int imgHeight)
-	{
-		const int med = (WS*WS)/2;
-		float window[WS*WS];
-		const int edgex = WS/2; // window width / 2
-		const int edgey = WS/2; // window height / 2
-
-		const int x_thread_offset = BW * blockIdx.x + threadIdx.x;
-		const int y_thread_offset = BH * blockIdx.y + threadIdx.y;
-
-		const int x_stride = BW * gridDim.x;
-		const int y_stride = BH * gridDim.y;
+	s = cuda.Event()
+	e = cuda.Event()
+	s.record()
 
 
-		for (int x = edgex + x_thread_offset; x < imgWidth - edgex; x += x_stride)
+	code = """
+
+		// X is the 2D image
+		const int BW = %(BW)s;
+		const int BH = %(BH)s;
+		const int WS = %(WS)s;
+
+		__global__ void mf(float* in, float* out, int imgWidth, int imgHeight)
 		{
-			for (int y = edgey + y_thread_offset; y < imgWidth - edgey; y += y_stride)
-			{
-				int i = 0;
-				for (int fx = 0; fx < WS; ++fx)
-				{
-					for (int fy = 0; fy < WS; ++fy)
-					{
-						window[i] = in[(x + fx - edgex)*imgWidth + (y + fy - edgey)];
-						i += 1;
-					}
-				}
+			const int med = (WS*WS)/2;
+			float window[WS*WS];
+			const int edgex = WS/2; // window width / 2
+			const int edgey = WS/2; // window height / 2
 
-				// Sort to find the median
-				for (int j = 0; j < WS*WS; ++j)
+			const int x_thread_offset = BW * blockIdx.x + threadIdx.x;
+			const int y_thread_offset = BH * blockIdx.y + threadIdx.y;
+
+			const int x_stride = BW * gridDim.x;
+			const int y_stride = BH * gridDim.y;
+
+
+			for (int x = edgex + x_thread_offset; x < imgWidth - edgex; x += x_stride)
+			{
+				for (int y = edgey + y_thread_offset; y < imgWidth - edgey; y += y_stride)
 				{
-					for (int k = j + 1; k < WS*WS; ++k)
+					int i = 0;
+					for (int fx = 0; fx < WS; ++fx)
 					{
-						if (window[j] > window[k])
+						for (int fy = 0; fy < WS; ++fy)
 						{
-							float tmp = window[j];
-							window[j] = window[k];
-							window[k] = tmp;
+							window[i] = in[(x + fx - edgex)*imgWidth + (y + fy - edgey)];
+							i += 1;
 						}
 					}
+
+					// Sort to find the median
+					for (int j = 0; j < WS*WS; ++j)
+					{
+						for (int k = j + 1; k < WS*WS; ++k)
+						{
+							if (window[j] > window[k])
+							{
+								float tmp = window[j];
+								window[j] = window[k];
+								window[k] = tmp;
+							}
+						}
+					}
+
+					out[x*imgWidth + y] = window[med];
+
 				}
-
-				out[x*imgWidth + y] = window[med];
-
 			}
 		}
-	}
-	"""
+		"""
 
-code = code % {
-		'BW' : BLOCK_WIDTH,
-		'BH' : BLOCK_HEIGHT,
-		'WS' : WINDOW_SIZE,
-	}
+	code = code % {
+			'BW' : BLOCK_WIDTH,
+			'BH' : BLOCK_HEIGHT,
+			'WS' : WINDOW_SIZE,
+		}
 
-mod = SourceModule(code)
-mf = mod.get_function('mf')
-
+	mod = SourceModule(code)
+	mf = mod.get_function('mf')
 
 
 
-indata = np.pad(indata_orig, padding, 'constant', constant_values=0)
-outdata = np.ones_like(indata)
+
+	indata = np.pad(indata, padding, 'constant', constant_values=0)
+	outdata = np.ones_like(indata)
 
 
-in_pin = cuda.register_host_memory(indata)
-out_pin = cuda.register_host_memory(outdata)
+	in_pin = cuda.register_host_memory(indata)
+	out_pin = cuda.register_host_memory(outdata)
 
-in_gpu = cuda.mem_alloc(indata.nbytes)
-out_gpu = cuda.mem_alloc(outdata.nbytes)
+	in_gpu = cuda.mem_alloc(indata.nbytes)
+	out_gpu = cuda.mem_alloc(outdata.nbytes)
 
-cuda.memcpy_htod(in_gpu, in_pin)
-# Check to see if this isn't needed because it is empty
-cuda.memcpy_htod(out_gpu, out_pin)
+	cuda.memcpy_htod(in_gpu, in_pin)
+	# Check to see if this isn't needed because it is empty
+	cuda.memcpy_htod(out_gpu, out_pin)
 
-mf.prepare("PPii")
+	mf.prepare("PPii")
 
-gridx = int(np.ceil((N+2*padding)/BLOCK_WIDTH))
-gridy = int(np.ceil((N+2*padding)/BLOCK_HEIGHT))
-grid = (gridx,gridy)
+	expanded_size = N + (2 * padding)
 
-mf.prepared_call( grid, (BLOCK_WIDTH, BLOCK_HEIGHT, 1), in_gpu, out_gpu, N+2*padding, N+2*padding)
-
-
-cuda.memcpy_dtoh(out_pin, out_gpu)
-
-if (padding > 0):
-	out_pin = out_pin[padding:-padding, padding:-padding]
+	gridx = max(1, int(np.ceil((expanded_size)/BLOCK_WIDTH)))
+	gridy = max(1, int(np.ceil((expanded_size)/BLOCK_HEIGHT)))
+	grid = (gridx,gridy)
+	block = (BLOCK_WIDTH, BLOCK_HEIGHT, 1)
+	mf.prepared_call(grid, block, in_gpu, out_gpu, expanded_size, expanded_size)
 
 
-e.record()
-e.synchronize()
-print s.time_till(e), "ms"
+	cuda.memcpy_dtoh(out_pin, out_gpu)
+
+	if (padding > 0):
+		out_pin = out_pin[padding:-padding, padding:-padding]
+
+	return out_pin
+
+	e.record()
+	e.synchronize()
+	print s.time_till(e), "ms"
 
 
-s.record()
-true_ans= sps.medfilt2d(indata_orig, (WINDOW_SIZE, WINDOW_SIZE))
-e.record()
-e.synchronize()
-print s.time_till(e), "ms"
+	# s.record()
+	# true_ans= sps.medfilt2d(indata, (WINDOW_SIZE, WINDOW_SIZE))
+	# e.record()
+	# e.synchronize()
+	#print s.time_till(e), "ms"
 
-print np.allclose(out_pin, true_ans)
-print out_pin
-print true_ans
+	#return np.allclose(out_pin, true_ans)
+	#print out_pin
+	#print true_ans
---
+++ mf_test_basic.py
@@ -0,0 +1,42 @@
+import unittest
+import numpy as np 
+import scipy.signal as sps
+import mf_redux
+
+class BasicTest(unittest.TestCase):
+	def testOneByOne(self):
+		""" Test using a 1x1 window """
+		in0 = np.random.rand(1, 1)
+		in1 = np.random.rand(29, 29)
+		in2 = np.random.rand(93, 93)
+		in3 = np.random.rand(7000, 7000)
+
+		check0 = sps.medfilt2d(in0, (1,1))
+		check1 = sps.medfilt2d(in1, (1,1))
+		check2 = sps.medfilt2d(in2, (1,1))
+		check3 = sps.medfilt2d(in3, (1,1))
+
+		self.assertTrue(np.allclose(check0, mf_redux.MedianFilter(ws=1, n=1, indata=in0)))
+		self.assertTrue(np.allclose(check1, mf_redux.MedianFilter(ws=1, n=29, indata=in1)))
+		self.assertTrue(np.allclose(check2, mf_redux.MedianFilter(ws=1, n=93, indata=in2)))
+		self.assertTrue(np.allclose(check3, mf_redux.MedianFilter(ws=1, n=7000, indata=in3)))
+
+	def testThreeByThree(self):
+		""" Test using a 3x3 window """
+		in0 = np.random.rand(1, 1)
+		in1 = np.random.rand(29, 29)
+		in2 = np.random.rand(93, 93)
+		in3 = np.random.rand(7000, 7000)
+
+		check0 = sps.medfilt2d(in0, (3,3))
+		check1 = sps.medfilt2d(in1, (3,3))
+		check2 = sps.medfilt2d(in2, (3,3))
+		check3 = sps.medfilt2d(in3, (3,3))
+
+		self.assertTrue(np.allclose(check0, mf_redux.MedianFilter(ws=3, n=1, indata=in0)))
+		self.assertTrue(np.allclose(check1, mf_redux.MedianFilter(ws=3, n=29, indata=in1)))
+		self.assertTrue(np.allclose(check2, mf_redux.MedianFilter(ws=3, n=93, indata=in2)))
+		self.assertTrue(np.allclose(check3, mf_redux.MedianFilter(ws=3, n=7000, indata=in3)))
+
+if __name__ == '__main__':
+	unittest.main()
