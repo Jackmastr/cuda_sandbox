12d97d6f25460cf5880beedeb346ddac4318bba1
Jackson <jsipple@berkeley.edu>
2018-07-02 11:50:55 -0700

    Decided to use CUBLAS (so need gpu arrays), it seems like there may have to be a lot more back-and-forth between host and device than I want to see, hopefully I can find a way in the maxiter for loop to minimize this

--- deconvolution/CLEAN.py
+++ deconvolution/CLEAN.py
@@ -7,6 +7,9 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy as np 
 import skcuda
+from skcuda import cublas
+import pycuda.gpuarray as gpuarray
+from math import sqrt
 
 def clean(res, ker, mdl=None, area=None, gain=0.1, maxiter=10000, tol=1e-3, stop_if_div=1, blockDimX=512):
 	s = cuda.Event()
@@ -39,15 +42,25 @@ def clean(res, ker, mdl=None, area=None, gain=0.1, maxiter=10000, tol=1e-3, stop
 	#grid=(1,1,1)
 
 	code = """
-		__global__ void compute_res(float *res, float *ker, float step, float *square_res)
+		__global__ void compute_res(float *res, float *ker, float step, float *square_res, int* area)
 		{
-			int index = threadIdx.x + blockDim.x * blockIdx.x;
-			if (index < %(DIM)s)
+			const int index = threadIdx.x + blockDim.x * blockIdx.x;
+			if (index < %(DIM)s && area[index])
 			{
 				int wrapped_index = index %% %(DIM)s;
 				res[wrapped_index] -= ker[index] * step;
 				res_at_wrapped = res[wrapped_index];
-				square_res_gpu[wrapped_index] = res_at_wrapped * res_at_wrapped;
+				square_res[wrapped_index] = res_at_wrapped * res_at_wrapped;
+			}
+		}
+
+		__global__ void square_ker(float *ker, float *square_ker)
+		{
+			const int index = threadIdx.x + blockDim.x * blockIdx.x;
+			if (index < %(DIM)s && area[index])
+			{
+				int temp = ker[index];
+				square_ker[index] = temp * temp;
 			}
 		}
 
@@ -55,7 +68,7 @@ def clean(res, ker, mdl=None, area=None, gain=0.1, maxiter=10000, tol=1e-3, stop
 	"""
 	code = code % {
 		'DIM': dim,
-		'GAIN': gain,
+		'BX': blockDimX,
 	}
 
 	mod = SourceModule(code)
@@ -73,20 +86,76 @@ def clean(res, ker, mdl=None, area=None, gain=0.1, maxiter=10000, tol=1e-3, stop
 	area_gpu = cuda.mem_alloc(area.nbytes)
 
 	# Additional buffers that may be needed
-	square_res_gpu = cuda.mem_alloc(res.nbytes)
+	square_res_gpu = gpuarray.empty_like(res_gpu)
+	square_ker_gpu = gpuarray.empty_like(ker_gpu)
 
 	cuda.memcpy_htod(res_gpu, res_pin)
 	cuda.memcpy_htod(ker_gpu, ker_pin)
 	cuda.memcpy_htod(mdl_gpu, mdl_pin)
 	cuda.memcpy_htod(area_gpu, area_pin)
 
+	# Variables needed on the host side
+	firstscore = -1
+	score = -1
+	best_score = -1
+	argmax = 0
+	max = 0
+
 	# Compute the gain/phase of the kernel to start out
+	h = cublasCreate()
 
-	for n in xrange(maxiter):
+	square_ker(ker_gpu, square_ker_gpu)
+	ker_argmax = cublas.cublasIsamax(h, square_ker_gpu, square_ker_gpu.size, square_ker_gpu.gpudata, 1)
+	q = 1/ker_pin[ker_max]
 
-		compute_res(res_gpu, ker_gpu, step, square_res_gpu)
+	n = 0
+	while n < maxiter:
 
-		square_res_gpu = skcuda.misc.cumsum(square_res_gpu)
+		nscore = 0
+		step = gain * max * q
+
+		mdl[argmax] += step
+
+		compute_res(res_gpu, ker_gpu, step, square_res_gpu, area_gpu)
+
+		nscore = cublas.cublasSasum(h, square_res_gpu.size, square_res_gpu.gpudata, 1)
+
+		# Check for divergence
+		nscore = sqrt(nscore/dim)
+		if firstscore < 0:
+			firstscore = score
+		if score > 0 and nscore > score:
+			if stop_if_div:
+				# Diverged ---> so undo and quit
+
+			elif best_score < 0 or score < best_score:
+				# Diverged ---> buf prev score in case maximum
+
+				# Reset maxiter counter
+				n = 0
+
+		elif score > 0 and (score - nscore) / firstscore < tol:
+			# We are done
+			cuda.memcpy_dtoh(res_pin, res_gpu)
+			cuda.memcpy_dtoh(mdl_pin, mdl_gpu)
+			return res_pin, res_gpu
+
+		elif !stop_if_div and (best_score < 0 || nscore < best_score):
+			# Reset maxiter counter
+			n = 0
+
+		# Find new max and update model
+		nargmax = cublas.cublasIsamax(h, square_res_gpu.size, square_res_gpu.gpudata, 1)
+		
+		""" PROBABLY A FASTER WAY THAN THIS """
+		cuda.memcpy_dtoh(res_pin, res_gpu)
+		max = res_pin[nargmax]
+
+		score = nscore
+		argmax = nargmax
+		n++
 
 
-
+	cuda.memcpy_dtoh(res_pin, res_gpu)
+	cuda.memcpy_dtoh(mdl_pin, mdl_gpu)
+	return res_pin, res_gpu
